e40c1717d6f7e167aae0562acbc88060eceaf1a3
Contestant <contestant@isucon.net>
2021-06-16 19:52:17 +0900

    cache on

--- sql/01_schema.sql
+++ sql/01_schema.sql
@@ -8,12 +8,12 @@ CREATE TABLE `users` (
   `staff`      BOOLEAN NOT NULL DEFAULT false,
   `created_at` DATETIME(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
-
 DROP TABLE IF EXISTS `schedules`;
 CREATE TABLE `schedules` (
   `id`         VARCHAR(255) PRIMARY KEY NOT NULL,
   `title`      VARCHAR(255) NOT NULL DEFAULT '',
   `capacity`   INT UNSIGNED NOT NULL DEFAULT 0,
+  `reserved`   INT UNSIGNED NOT NULL DEFAULT 0,
   `created_at` DATETIME(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
